d73f39872527b16cd9bd74d387ccfa4279705c5d
fatemeh-azimi <65613115+fatemeh-azimi@users.noreply.github.com>
2023-08-04 08:02:58 -0700

    swagger

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,23 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.documentation import include_docs_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Blog Api",
+        default_version="v1",
+        description="this is a test api for Azimi project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="f.s.azimi.2001@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny] #میپرسد که چه کسانی میتوانند این صفحه داکیومنت را ببینند؟ 
+)
 
 
 urlpatterns = [
@@ -12,7 +29,12 @@ urlpatterns = [
 
     path('api-auth/', include('rest_framework.urls')),
 
-    path('api-docs/', include_docs_urls(title='api sample'))
+    path('api-docs/', include_docs_urls(title='api sample')),
+
+    path("swagger/output.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+
    ]
 
 # serving static and media for development
